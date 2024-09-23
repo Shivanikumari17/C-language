8d62a0d09a88d9fc087fd2a95b6555376e90a00d
Shivanikumari17 <kumarishiv678@gmail.com>
2024-09-23 22:12:50 +0530

    whether a number is +ve,-ve & zero.C

---
+++ Check whether a number is +ve,-ve & zero.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int number;
+printf("enter a number");
+scanf("%d", &number);
+if (number>0) {
+printf("number is positive");
+}
+if(number<0){
+printf("number is negative"); }
+}
