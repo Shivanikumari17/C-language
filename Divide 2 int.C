a640be9832e45a00505528af115ba8855d8d4020
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-02 21:34:11 +0530

    Divide 2 int.C

---
+++ Divide 2 int.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int a;
+ int b;
+ int c;
+ printf("First num\n");
+ scanf("%d", &a);
+ printf("Second num\n");
+ scanf("%d", &b);
+ c = a / b;
+ printf("%d", c);
+    return 0;
+    }
