9675a3b9ee720008443eb1426445a9b336e0e782
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-02 21:44:39 +0530

    Subtract.C

---
+++ Subtract.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ int main(void) {
+     char str1[] = "abcd";
+     char str2[] = "xyz";
+
+     int x = str1 - str2;
+
+     printf("Subtraction diff: %d\n", x);
+
+     return 0;
+ }
