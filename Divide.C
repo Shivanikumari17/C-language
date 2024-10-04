b0aba3189c0c121c15b056b6797fa1b273a4ca24
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-04 21:33:50 +0530

    Divide.C

---
+++ Divide.C
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main(){ 
+  int a,b, divide;
+ printf ("enter the value of a"); 
+  scanf("%d",&a);
+  printf("enter the value of b");
+  scanf("%d",&b);
+divide=a/b;
+  printf("the multiplication of %d and%d is %d\n",a,b,divide);
+  return 0;
+  }
