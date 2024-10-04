2546e95ee19ed3a1cad20933e897ca296a840000
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-04 21:56:50 +0530

    Perimeter of square.C

---
+++ Perimeter of square.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+  int main() {
+  int side, perimeter;
+  printf("Enter the Length of Side: "); scanf("%d", &side);
+  perimeter = 4 * side;
+  printf("perimeter of Square: %d", perimeter);
+
+  return (0);
+}
