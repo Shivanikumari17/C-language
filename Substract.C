ed273e127e17138edec2e8f12215ad89e8a16e41
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-04 21:40:30 +0530

    Substract.C

---
+++ Substract.C
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main(){ 
+  int a,b, substract;
+ printf ("enter the value of a"); 
+  scanf("%d",&a);
+  printf("enter the value of b");
+  scanf("%d",&b);
+substract=a-b;
+  printf("the substract of %d and%d is %d\n",a,b,substract);
+  return 0;
+  }
