b0a6dcb35af8bddd62de2fa0e7eef5e85810a230
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-04 21:30:48 +0530

    Multiply.C

---
+++ Multiply.C
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main(){ 
+  int a,b, multiply;
+ printf ("enter the value of a"); 
+  scanf("%d",&a);
+  printf("enter the value of b");
+  scanf("%d",&b);
+multiply=a*b;
+  printf("the multiplication of %d and%d is %d\n",a,b,multiply);
+  return 0;
+  }
