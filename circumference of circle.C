dd9657b44c357a7cbe409b2d968026d15ff86487
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-05 19:36:25 +0530

    circumference of circle.C

---
+++ circumference of circle.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+  int main() {
+  int r, circumference ;
+  printf("Enter the Length of r: "); scanf("%d", &r);
+  circumference = 2*3.14*r;
+  printf(" circumference of circle: %d", circumference);
+
+  return 0;
+}
