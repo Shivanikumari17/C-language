ff3bb27a9e182689e09e80ab3b0138833416891c
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-05 20:40:38 +0530

    Diameter of circle.C

---
+++ Diameter of circle.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+  int main() {
+  int r,diameter ;
+  printf("Enter the Length of r "); scanf("%d", &r); 
+    diameter= 2*r;
+  printf(" diameter of circle: %d" ,diameter);
+
+  return 0;
+}
