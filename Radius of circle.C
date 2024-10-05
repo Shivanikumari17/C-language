e9ceab1a02d9fd400b716d74e62aae709fe65d97
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-05 20:44:41 +0530

    Radius of circle.C

---
+++ Radius of circle.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+  int main() {
+  int d, radius;
+  printf("Enter the Length of d "); scanf("%d", &d); 
+    radius= d/2;
+  printf(" radius of circle: %d" ,radius);
+
+  return 0;
+}
