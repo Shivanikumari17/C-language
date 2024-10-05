f79da7f1494f742929d6e7fc0924cf7f929e41d2
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-05 19:47:38 +0530

    Perimeter of rectangle.C

---
+++ Perimeter of rectangle.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int main() {
+  int l, b, perimeter ;
+  printf("Enter the Length of l "); scanf("%d", &l); 
+  printf(" Enter the value of b");
+    scanf("%d", &b);
+    perimeter = 2*(l+b);
+  printf(" perimeter of rectangle: %d", perimeter);
+
+  return 0;
+}
