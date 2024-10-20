8f7f901451ca83925909a6c55bd639184323efd0
Shivanikumari17 <kumarishiv678@gmail.com>
2024-10-20 05:56:25 +0530

    Vowels.C

---
+++ Vowels.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+char x,ch;
+printf("enter a character");
+scanf("%c",&x);
+switch(x)
+  {
+    case 'a': printf("vowel");
+  break;
+    case 'e': printf("vowel");
+    break;   
+      case'i': printf("vowel");
+    break;
+    case'o': printf("vowel");
+    break;
+    case'u': printf("vowel");
+    break;
+    default: printf("c");
+    break;
+  }
+    }
